9a3f7c5b61ce073d805d42b733cbf29ab156113e
Carlos Quijano <carlos@crqgestion.es>
2014-06-10 13:39:37 +0200

    added automatic package

---
+++ addons/service/downloadmanager/automatic/package.mk
@@ -0,0 +1,50 @@
+################################################################################
+################################################################################
+
+PKG_NAME="automatic"
+PKG_VERSION="0.8.3"
+PKG_REV="1"
+PKG_ARCH="any"
+PKG_LICENSE="GPL"
+PKG_SITE="http://kylek.is-a-geek.org:31337/Automatic"
+PKG_URL="https://github.com/1100101/Automatic/archive/v$PKG_VERSION.tar.gz"
+PKG_DEPENDS="pcre libxml2 curl"
+PKG_BUILD_DEPENDS="toolchain pcre libxml2 curl"
+PKG_PRIORITY="optional"
+PKG_SECTION="service/downloadmanager"
+PKG_SHORTDESC="Automatic torrent downloader"
+PKG_LONGDESC="Automatic torrent downloader"
+
+PKG_IS_ADDON="yes"
+PKG_ADDON_TYPE="xbmc.service"
+
+PKG_AUTORECONF="no"
+unpack(){
+        [ -d $BUILD/$PKG_NAME-$PKG_VERSION ] && rm -rf $BUILD/$PKG_NAME-$PKG_VERSION
+        mkdir -p $BUILD/$PKG_NAME-$PKG_VERSION
+        tar -xzf $SOURCES/$PKG_NAME/v$PKG_VERSION.tar.gz -C $BUILD/$PKG_NAME-$PKG_VERSION
+}
+
+pre_configure_target() {
+	#cd ./$BUILD/$PKG_NAME-$PKG_VERSION/Automatic-$PKG_VERSION
+	cd Automatic-$PKG_VERSION
+	./autogen.sh --host=$TARGET_NAME \
+             --build=$HOST_NAME \
+             --target=$TARGET_NAME 
+
+	gl_cv_func_malloc_0_nonnull=yes ac_cv_func_malloc_0_nonnull=yes \
+	ac_cv_func_calloc_0_nonnull=yes ac_cv_func_realloc_0_nonnull=yes \
+	./configure --host=$TARGET_NAME \
+            --build=$HOST_NAME \
+            --target=$TARGET_NAME 
+}
+
+make_target() {
+	make 
+}
+post_install() {
+mkdir -p $ADDON_BUILD/$PKG_ADDON_ID/bin
+  cp $BUILD/$PKG_NAME/src/automatic $ADDON_BUILD/$PKG_ADDON_ID/bin
+mkdir -p $ADDON_BUILD/$PKG_ADDON_ID/etc
+  cp $BUILD/$PKG_NAME/src/automatic.conf-sample $ADDON_BUILD/$PKG_ADDON_ID/etc/automatic.conf
+}
