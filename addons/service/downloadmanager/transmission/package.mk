024f73b8d653b6a197f652b5ba5f186747dc21e8
Carlos Quijano <carlos@crqgestion.es>
2015-03-03 18:13:30 +0100

    fix package.mk

--- addons/service/downloadmanager/transmission/package.mk
+++ addons/service/downloadmanager/transmission/package.mk
@@ -49,7 +49,6 @@ PKG_CONFIGURE_OPTS_TARGET="--enable-utp \
 
 post_install() {
 	mkdir -p $INSTALL/bin
-	echo "$PKG_BUILD/.$TARGET_NAME/daemon/transmission-daemon $INSTALL/bin/transmission-daemon"
 	cp $PKG_BUILD/.$TARGET_NAME/daemon/transmission-daemon $INSTALL/bin/transmission-daemon
 	cp $PKG_BUILD/.$TARGET_NAME/daemon/transmission-remote $INSTALL/bin/transmission-remote
 	mkdir -p $INSTALL/web
